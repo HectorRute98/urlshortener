d147f25258470d804091d2a7781a0d6319f02898
Davidzf21 <780500@unizar.es>
2022-11-29 16:57:23 +0100

    Update Exceptions.kt

--- core/src/main/kotlin/es/unizar/urlshortener/core/Exceptions.kt
+++ core/src/main/kotlin/es/unizar/urlshortener/core/Exceptions.kt
@@ -5,6 +5,7 @@ class InvalidUrlException(val url: String) : Exception("[$url] does not follow a
 class RedirectionNotFound(val key: String) : Exception("[$key] is not known")
 
 class UpdateValidationNoWork(val key: String) : Exception("[$key] validation can not be update")
+
 class RedirectionNotSafeOrBlock(val key: String) : Exception("Hash [$key] redirection block. Page is block or not safe")
 
 class ShortUrlNotSafe(val key: String) : Exception("Page [$key] is not safe")
